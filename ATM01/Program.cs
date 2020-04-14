920bd8522c2f84b208ee34b86b11588ddc77d0b6
GalbinitaSebastian97 <61518348+GalbinitaSebastian97@users.noreply.github.com>
2020-04-14 18:55:06 +0300

    Refactored withdrawl into separate method

--- ATM01/Program.cs
+++ ATM01/Program.cs
@@ -7,20 +7,25 @@ namespace ATM01
         static void Main(string[] args)
         {
             var debitCalculator = new DebitAccountWithdrawalFeeCalculator();
-            
+ 
             Account debitAccount = new DebitAccount();
             debitAccount.Deposit(100);
-            debitAccount.Withdraw(debitCalculator.CalculateAmountToWithDraw(debitAccount,50));
-            Console.WriteLine("Debit account :{0}.", debitAccount.Amount);
+            WithDrawFrom(debitAccount,50, debitCalculator);
 
 
             var creditCalculator = new CreditAccountWithdrawalFeeCalculator();
             Account creditAccount = new CreditAccount();
             creditAccount.Deposit(100);
-            creditAccount.Withdraw(creditCalculator.CalculateAmountToWithDraw(creditAccount, 50));
-            Console.WriteLine("Credit account:{0}.", creditAccount.Amount);
+            WithDrawFrom(creditAccount, 50, creditCalculator);
+            
 
 
+        }
+        static void WithDrawFrom(Account account, decimal amount, WithDrawalFeeCalculator withDrawalFeeCalculator)
+        {
+            var totalAmount = withDrawalFeeCalculator.CalculateAmountToWithDraw(account, amount);
+            account.Withdraw(totalAmount);
+            Console.WriteLine("{0}:{1}", account.GetType().Name, account.Amount);
         }
     }
 }
