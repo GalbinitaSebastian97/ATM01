124821259e19729c3b90ce44ad10abd286541627
GalbinitaSebastian97 <61518348+GalbinitaSebastian97@users.noreply.github.com>
2020-04-14 18:26:50 +0300

    Open Closure Principle

---
+++ ATM01/DebitAccountWithdrawalFeeCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace ATM01
+{
+    class DebitAccountWithdrawalFeeCalculator : WithDrawalFeeCalculator
+    {
+        protected override decimal CalculateComission(decimal amount)
+        {
+            return 0m;
+        }
+    }
+}
